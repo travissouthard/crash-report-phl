7727353a7a3bcc99c10da74ba610bc11e6f6ba44
Travis Southard <tsouthard88@gmail.com>
2020-07-08 16:37:15 -0400

    Added router controller for crashreports routes, cleaned up some double-vs-single quotes conistency

---
+++ controllers/crashreports.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const router = express.Router();
+const crashreports = require("../models/crashreports.js")
+
+// Routes
+
+//New
+router.get("/new" , (req, res) => {
+    res.render("new.ejs");
+});
+
+//Create
+router.post("/", (req, res) => {
+    // Convert location to specific coordinates
+        // Need API for this
+    
+    //Converts any checkbox into a boolean
+    const convertBoolean = (property) => {
+        if (property == "on") {
+            return true;
+        } else {
+            return false;
+        };
+    };
+    // Convert hitAndRun to true
+    req.body.hitAndRun = convertBoolean(req.body.hitAndRun);
+    // Convert called911 to true
+    req.body.called911 = convertBoolean(req.body.called911);
+    // Convert madeReport to true
+    req.body.madeReport = convertBoolean(req.body.madeReport);
+    // Convert haveLawyer to true
+    req.body.haveLawyer = convertBoolean(req.body.haveLawyer);
+    // Convert madeSuit to true
+    req.body.madeSuit = convertBoolean(req.body.madeSuit);
+
+    // If description is empty, convert to "Undisclosed"
+    if (req.body.description == "") {
+        req.body.description = "Undisclosed"
+    }
+
+    // If car, bike, or ped == "on": push into travel mode array
+    let mode = [];
+    const addMode = (option, string) => {
+        if (option == "on") {
+            mode.push(string);
+        };
+    };
+    addMode(req.body.car, "car");
+    addMode(req.body.bike, "bike");
+    addMode(req.body.ped, "ped");
+    req.body.mode = mode;
+    // Delete req.body.car, req.body.bike, & req.body.ped
+    delete req.body.car;
+    delete req.body.bike;
+    delete req.body.ped;
+
+    // If madereport == true and reportNumber is empty: reportNumber is "unavailable". If madeReport == false: report number = "doesn't exist"
+    if (req.body.madeReport == true && req.body.reportNumber == "") {
+        req.body.reportNumber = "Undisclosed";
+    } else if (req.body.madeReport == false) {
+        req.body.reportNumber = "No report";
+    };
+
+    // If haveLawyer == true and lawyerName is empty: lawyername is "undisclosed". If haveLawyer == false: lawyerName is "No lawyer"
+    if (req.body.haveLawyer == true && req.body.lawyerName == "") {
+        req.body.lawyerName = "Undisclosed";
+    } else if (req.body.haveLawyer == false) {
+        req.body.lawyerName = "No lawyer";
+    };
+
+    res.send(req.body);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 //___________________
 //Dependencies
 //___________________
-const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
+const express = require("express");
+const methodOverride  = require("method-override");
+const mongoose = require ("mongoose");
 const app = express();
 const db = mongoose.connection;
+const reportsController = require("./controllers/crashreports.js");
 
 //___________________
 //Port
@@ -35,84 +36,15 @@ db.on('open' , ()=>{});
 //___________________
 
 //use public folder for static assets
-app.use(express.static('public'));
+app.use(express.static("public"));
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
 app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 //use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-
-//___________________
-// Routes
-//___________________
-//Landing page
-app.get('/' , (req, res) => {
-    res.render('new.ejs');
-});
-
-//Create
-app.post("/crashreports", (req, res) => {
-    // Convert location to specific coordinates
-        // Need API for this
-    
-    //Converts any checkbox into a boolean
-    const convertBoolean = (property) => {
-        if (property == "on") {
-            return true;
-        } else {
-            return false;
-        };
-    };
-    // Convert hitAndRun to true
-    req.body.hitAndRun = convertBoolean(req.body.hitAndRun);
-    // Convert called911 to true
-    req.body.called911 = convertBoolean(req.body.called911);
-    // Convert madeReport to true
-    req.body.madeReport = convertBoolean(req.body.madeReport);
-    // Convert haveLawyer to true
-    req.body.haveLawyer = convertBoolean(req.body.haveLawyer);
-    // Convert madeSuit to true
-    req.body.madeSuit = convertBoolean(req.body.madeSuit);
-
-    // If description is empty, convert to "Undisclosed"
-    if (req.body.description == "") {
-        req.body.description = "Undisclosed"
-    }
-
-    // If car, bike, or ped == "on": push into mode array
-    let mode = [];
-    const addMode = (option, string) => {
-        if (option == "on") {
-            mode.push(string);
-        };
-    };
-    addMode(req.body.car, "car");
-    addMode(req.body.bike, "bike");
-    addMode(req.body.ped, "ped");
-    req.body.mode = mode;
-    // Delete req.body.car, req.body.bike, & req.body.ped
-    delete req.body.car;
-    delete req.body.bike;
-    delete req.body.ped;
-
-    // If madereport == true and reportNumber is empty: reportNumber is "unavailable". If madeReport == false: report number = "doesn't exist"
-    if (req.body.madeReport == true && req.body.reportNumber == "") {
-        req.body.reportNumber = "Undisclosed";
-    } else if (req.body.madeReport == false) {
-        req.body.reportNumber = "No report";
-    };
-
-    // If haveLawyer == true and lawyerName is empty: lawyername is "undisclosed". If haveLawyer == false: lawyerName is "No lawyer"
-    if (req.body.haveLawyer == true && req.body.lawyerName == "") {
-        req.body.lawyerName = "Undisclosed";
-    } else if (req.body.haveLawyer == false) {
-        req.body.lawyerName = "No lawyer";
-    };
-
-    res.send(req.body);
-})
+app.use(methodOverride("_method"));// allow POST, PUT and DELETE from a form
+app.use("/crashreports/", reportsController);
 
 //___________________
 //Listener
 //___________________
-app.listen(PORT, () => console.log( 'Listening on port:', PORT));
+app.listen(PORT, () => console.log("Listening on port:", PORT));
