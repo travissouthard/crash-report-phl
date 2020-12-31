aeede662867d3c832f01b526e016541091214776
Travis Southard <tsouthard88@gmail.com>
2020-12-31 17:53:28 -0500

    Moved mapbox script to bottom of report template.

--- public/js/app.js
+++ public/js/app.js
@@ -13,20 +13,5 @@ $(() => {
     });
     
     // Mapbox Things
-    mapboxgl.accessToken = "pk.eyJ1IjoidHJhdmlzc291dGhhcmQiLCJhIjoiY2tiaTNkODI5MGJhdjJ5bTh1ZGtzazdlNSJ9.a7G1oI8IkOxDuH8kBnEfcA";
-    let map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/travissouthard/ckjd6y5j624ll19phixshuo24', // stylesheet location
-        center: [-75.163597, 40.0], // starting position [lng, lat]
-        zoom: 9.25 // starting zoom
-    });
-    let marker = new mapboxgl.Marker({
-        draggable: true
-    }).setLngLat([-75.2032832, 39.9631334]).addTo(map);
-
-    marker.on("dragend", () => {
-        let coordObj = marker.getLngLat()
-        let lngLat = [coordObj.lng, coordObj.lat].join()
-        $("#lngLat").val(lngLat)
-    })
+    //Temporarily moved to bottom of report.js until a better way to keep json as json, probably needs to be a React frontend rather than templates
 });
