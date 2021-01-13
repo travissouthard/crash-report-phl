d8e33a418eccd2ff03c67b636968e1a76472d6e9
Travis Southard <tsouthard88@gmail.com>
2021-01-13 10:23:10 -0500

    Refactored the convertData function in the controller to be more readable. LIfted the convertBoolean function outside it and made a checkUndisclosed function to reduce repetition

--- controllers/crashreports.js
+++ controllers/crashreports.js
@@ -42,34 +42,29 @@ const writeDateBetter = (date) => {
 // Data Handler
 //=============
 
-// Callback for converting form data to useable data
-const convertData = (data) => {
-    //Converts any checkbox into a boolean
-    const convertBoolean = (property) => {
-        if (property == "on") {
-            return true;
-        } else {
-            return false;
-        };
+const convertBoolean = (property) => {
+    return property == "on";
+};
+
+let checkUndisclosed = (string, boolean=true) => {
+    if (boolean == true && string == "") {
+        return "Undisclosed";
+    } else if (boolean == false) {
+        return "None";
+    } else {
+        return string;
     };
-    // Convert hitAndRun to true
+}
+
+const convertData = (data) => {
     data.hitAndRun = convertBoolean(data.hitAndRun);
-    // Convert called911 to true
     data.called911 = convertBoolean(data.called911);
-    // Convert madeReport to true
     data.madeReport = convertBoolean(data.madeReport);
-    // Convert haveLawyer to true
     data.haveLawyer = convertBoolean(data.haveLawyer);
-    // Convert madeSuit to true
     data.madeSuit = convertBoolean(data.madeSuit);
-
-    // //Splits the lat and long into an array for mapping
-    // data.latLong = data.latLong.split(",");
-
-    // If description is empty, convert to "Undisclosed"
-    if (data.description == "") {
-        data.description = "Undisclosed"
-    }
+    data.description = checkUndisclosed(data.description)
+    data.reportNumber = checkUndisclosed(data.reportNumber, data.madeReport)
+    data.lawyerName = checkUndisclosed(data.lawyerName, data.haveLawyer)
 
     // If car, bike, or ped == "on": push into travel mode array
     let mode = [];
@@ -82,24 +77,10 @@ const convertData = (data) => {
     addMode(data.bike, "bike");
     addMode(data.ped, "ped");
     data.mode = mode;
-    // Delete req.body.car, req.body.bike, & req.body.ped
+
     delete data.car;
     delete data.bike;
     delete data.ped;
-
-    // If madereport == true and reportNumber is empty: reportNumber is "unavailable". If madeReport == false: report number = "doesn't exist"
-    if (data.madeReport == true && data.reportNumber == "") {
-        data.reportNumber = "Undisclosed";
-    } else if (data.madeReport == false) {
-        data.reportNumber = "No report";
-    };
-
-    // If haveLawyer == true and lawyerName is empty: lawyername is "undisclosed". If haveLawyer == false: lawyerName is "No lawyer"
-    if (data.haveLawyer == true && data.lawyerName == "") {
-        data.lawyerName = "Undisclosed";
-    } else if (data.haveLawyer == false) {
-        data.lawyerName = "No lawyer";
-    };
 
     return data;
 }
@@ -132,10 +113,6 @@ router.get("/new", (req, res) => {
 
 //Create
 router.post("/", (req, res) => {
-    // Convert location to specific coordinates
-    // Need API for this
-
-    //Convert data with callback
     req.body = convertData(req.body);
     Reports.create(req.body, (err, report) => {
         if (err) {
@@ -170,7 +147,7 @@ router.get("/:id/edit", (req, res) => {
 router.put("/:id", (req, res) => {
     //Convert data with callback
     req.body = convertData(req.body);
-
+    console.table(req.body)
     Reports.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, newReport) => {
         if (err) {
             console.log(err);
