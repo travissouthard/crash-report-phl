3de34e0186924838ededf80c7e3820cb8da3a520
Travis Southard <tsouthard88@gmail.com>
2021-01-14 09:53:43 -0500

    Cleaned up spacing and comments in public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -12,22 +12,24 @@ $(() => {
         $("#lawyerInfo").toggleClass("hidden");
     });
     
-    // Mapbox Things
+    //Mapbox Functionality
     mapboxgl.accessToken = "pk.eyJ1IjoidHJhdmlzc291dGhhcmQiLCJhIjoiY2tiaTNkODI5MGJhdjJ5bTh1ZGtzazdlNSJ9.a7G1oI8IkOxDuH8kBnEfcA";
     let map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/travissouthard/ckjd6y5j624ll19phixshuo24', // stylesheet location
-        center: [-75.163597, 40.0], // starting position [lng, lat]
-        zoom: 9.25 // starting zoom
+        style: 'mapbox://styles/travissouthard/ckjd6y5j624ll19phixshuo24',
+        center: [-75.163597, 40.0],
+        zoom: 9.25
     });
 
     let coords;
     
     if ($("#lngLat").val()) {
         coords = $("#lngLat").val().split(",")
+        
         let marker = new mapboxgl.Marker({
             draggable: true
         }).setLngLat(coords).addTo(map);
+        
         marker.on("dragend", () => {
             let coordObj = marker.getLngLat();
             let lngLat = [coordObj.lng, coordObj.lat].join();
@@ -35,6 +37,7 @@ $(() => {
         })
     } else if ($(".lngLat").length > 0) {
         let markers = [];
+        
         $(".lngLat").map(i => {
             coords = $(".lngLat")[i].id.split(",");
             let marker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
